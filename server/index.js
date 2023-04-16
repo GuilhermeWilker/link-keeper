41149b663611410c7e9756fe252c685a5db74459
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-04-16 14:43:02 -0300

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+const PORT = 3000;
+
+// Inicializando o Servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta: ${PORT}`);
+});
