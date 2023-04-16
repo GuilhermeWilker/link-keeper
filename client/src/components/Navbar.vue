dd54081cecb9cb272f630a4bb18f6fb8eb795ba9
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-04-16 15:09:45 -0300

    feat: Configuração de ambiente & Criação de componente e estilização [navBar]

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <h3 class="logo">Link keeper</h3>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-link class="btn_cta" to="/">Começar hoje</router-link>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 15px 20px;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 120px;
+
+  padding: 13px;
+  width: 300px;
+
+  background: #ffffff;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.25);
+  border-radius: 3px;
+}
+
+nav a:hover {
+  font-weight: 600;
+}
+
+.btn_cta {
+  padding: 15px 20px;
+  color: white;
+  background-color: #222222;
+  transition: 0.2s;
+}
+
+.btn_cta:hover {
+  background-color: #333333;
+}
+</style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,33 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <section class="hero">
+      <div class="text_content">
+        <h4 class="subtitle">O melhor lugar para salvar links de:</h4>
+        <h1 class="title">ARTIGOS</h1>
+      </div>
+    </section>
   </main>
 </template>
+
+<style scoped>
+main {
+  margin: 0 auto;
+  width: 100%;
+  border: 1px solid black;
+  padding: 30px;
+}
+.hero {
+  width: 100%;
+}
+
+.text_content {
+  margin: 0 auto;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 500;
+}
+</style>
