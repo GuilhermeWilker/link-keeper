d3451301ba5dfb58a97e0f52fc4a13b5c5b6df7f
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-04-16 21:49:40 -0300

    feat: Configuração e conexão com Banco de dados & Definindo rotas e servidor backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+require("dotenv").config();
 const mysql = require("mysql");
 
 const app = express();
 const PORT = 3000;
 
-const mensagem = "API funcionando";
+// Configuração e Conexão com Banco de Dados MySQL
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
+
+db.connect((err, results) => {
+  if (err) throw err;
+  console.log("Banco de dados conectado com sucesso!!");
+});
 
 // Definindo rotas
 app.get("/teste", (req, res) => {
